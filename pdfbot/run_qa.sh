ef3445c80654683a17c08001b1dff988c21993e7
webdevserv <60757009+webdevserv@users.noreply.github.com>
2024-12-09 21:08:13 +0100

    Delete pdfbot/run_qa.sh

--- pdfbot/run_qa.sh
+++
@@ -1,9 +0,0 @@
-PROJECT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-cd "$PROJECT_DIR"
-
-export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
-export STREAMLIT_RUNONSSAVE=True
-pip install -r requirements.txt
-
-streamlit run app.py --server.port 8787 --browser.serverAddress localhost
